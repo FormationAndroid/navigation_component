71d52fc384089bdba59b06fa4dd0c0a424b532e9
Jérôme Clament-Sanz <j.clamentsanz@gmail.com>
2020-06-08 11:29:34 +0200

    first commit

---
+++ app/src/main/java/com/example/navigationapp/fragments/SecondFragment.kt
@@ -0,0 +1,31 @@
+package com.example.navigationapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.navigationapp.R
+import kotlinx.android.synthetic.main.fragment_second.*
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        btnBackFirst.setOnClickListener {
+            view.findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+
+    }
+
+}
